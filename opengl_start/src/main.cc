2f4e32e9c934e7d730912fdf0576e9ca805c5574
soerenfi <soeren.fi@icloud.com>
2021-10-17 01:01:10 +0200

    add glew

--- opengl_start/src/main.cc
+++ opengl_start/src/main.cc
@@ -1,42 +1,57 @@
+#include "GL/glew.h"
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <stdio.h>
 
-int main(void)
-{
-    GLFWwindow* window;
+int main(void) {
+  GLFWwindow *window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+  /* Initialize the library */
+  if (!glfwInit())
+    return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+  //   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  //   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  //   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  //   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f,0.5f);
-        glVertex2f(0.0f,0.5f);
-        glVertex2f(-0.5f,-0.5f);
-        glEnd();
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window) {
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW");
+    return -1;
+  }
+
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
+  unsigned int a;
+  glGenBuffers(1, &a);
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window)) {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5f, 0.5f);
+    glVertex2f(0.0f, 0.5f);
+    glVertex2f(-0.5f, -0.5f);
+    glEnd();
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }

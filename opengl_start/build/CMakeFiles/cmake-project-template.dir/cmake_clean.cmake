33fee8ccf37bde608b3151d7c7ac12113659285b
Jana <Jana@Janas-MacBook.local>
2021-10-17 00:16:21 +0200

    macos opengl template

---
+++ opengl_start/build/CMakeFiles/cmake-project-template.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake-project-template.dir/src/main.cc.o"
+  "CMakeFiles/cmake-project-template.dir/src/main.cc.o.d"
+  "cmake-project-template"
+  "cmake-project-template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake-project-template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ opengl_start/build/CMakeFiles/cmake-project-template.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cmake-project-template.
+# This may be replaced when dependencies are built.
